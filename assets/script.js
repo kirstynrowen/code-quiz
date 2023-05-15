f3cc3c28d5c23c9fb2715600210c332d6763c07f
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-05-14 18:53:15 -0700

    pseudocode and htmloutline

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+//     button element in HTML, event listener in JS 
+// THEN a timer starts and I am presented with a question
+//     array of questions/answers
+//     function to start game
+//     function to update timer
+//     setInterval function to decrement time and update, HTML element to display time left
+// WHEN I answer a question
+//     selectable answer buttons
+// THEN I am presented with another question
+//     show/hide HTML elements?
+// WHEN I answer a question incorrectly
+//     Need some way to verify question answers
+// THEN time is subtracted from the clock
+//     decrement time by 10 seconds?
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+//     gameOver function
+// WHEN the game is over
+// THEN I can save my initials and my score
+//     local storage
+
+
+// QUESTIONS: 
+// 1. What is the index of the first element in an array?
+
+// 2. What does the "push" method do?
+//     adds new element to beginning of array
+//     copies an array
+//     adds new element to end of array
+//     other
+
+// 3. 
